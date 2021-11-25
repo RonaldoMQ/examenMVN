3ee98f36b0c89f0dc10efd3a7a0c3b9b2e6665a9
RonaldoMQ <mamanipari@gmail.com>
2021-11-24 21:40:32 -0500

    examen de la segunda unidad

---
+++ ExamenRMQ/src/test/java/pe/edu/upeu/rmq/App.java
@@ -0,0 +1,20 @@
+package pe.edu.upeu.rmq;
+
+import pe.edu.upeu.rmq.examen.ResolucionExamen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        ResolucionExamen Obj = new ResolucionExamen();
+
+        // Obj.impuestoAutos();
+        // Obj.TMultiplicar1hasta20();
+        // Obj.numerosperfectos();
+        System.out.println(Obj.potencia(3, 5));
+
+    }
+}
